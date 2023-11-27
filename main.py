21d338ba0c482f824a915ae5908fa2ae2d460d28
Níckolas Javier Santos Lívero <njsl.snf21@uea.edu.br>
2023-11-27 18:54:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,32 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import linear_kernel
+from sklearn.metrics.pairwise import cosine_similarity
 import streamlit as st
 
-# Carregar o dataset localmente
 file_path = 'dataset.csv'
 df = pd.read_csv(file_path)
 
-# Processamento dos dados (tratamento de NaN, se necessário)
-# Exemplo: df['book title'].fillna('', inplace=True)
 
-# Vetorização do texto
+df['combined_features'] = df['book title'] + ' ' + df['author'] + ' ' + df['genre']
+
 tfidf = TfidfVectorizer(stop_words='english')
-tfidf_matrix = tfidf.fit_transform(df['book title'])
+tfidf_matrix = tfidf.fit_transform(df['combined_features'])
 
-# Cálculo da matriz de similaridade (usando similaridade do cosseno)
-cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
 
-# Função para obter recomendações de livros com base no título inserido
 def get_recommendations(book_title):
-    # Verificar se o título do livro está no dataset
-    if book_title not in df['book title'].values:
-        return "Livro não encontrado. Tente outro título."
-
     idx = df[df['book title'] == book_title].index[0]
     sim_scores = list(enumerate(cosine_sim[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
     sim_scores = sim_scores[1:6]  # Recomendar os 5 livros mais similares
     book_indices = [i[0] for i in sim_scores]
-    return df.iloc[book_indices]['book title']
+    return df.iloc[book_indices][['book title', 'author', 'genre', 'year of publication', 'rating']]
 
-# Interface Streamlit
+
 st.title('Sistema de Recomendação de Livros')
 
-book_input = st.text_input('Digite o nome de um livro:', 'The Great Gatsby')  # Exemplo de entrada padrão
+selected_book = st.selectbox('Selecione um livro:', df['book title'].unique())
+
 if st.button('Obter Recomendações'):
-    recommendations = get_recommendations(book_input)
-    st.write(recommendations)
+    recommendations = get_recommendations(selected_book)
+    st.table(recommendations)
