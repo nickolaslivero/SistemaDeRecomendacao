9405ca209168abbe09305c9e7f7a4fe3fd5ded65
nickolaslivero <njsl.snf21@uea.edu.br>
2023-11-30 17:33:38 -0400

    colaborative4

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import streamlit as st
 from sklearn.metrics.pairwise import euclidean_distances
 from sklearn.preprocessing import MinMaxScaler
 
-
 file_path = 'dataset_avaliacoes.csv'
 df = pd.read_csv(file_path)
 
@@ -16,14 +15,21 @@ st.title("Sistema de Filtragem Colaborativa")
 
 selected_movie = st.selectbox("Selecione um filme:", df['Filme'].unique())
 
-def get_recommendations_euclidean(movie_name, similarity_matrix):
+def get_recommendations_euclidean(movie_name, similarity_matrix, pivot_table):
     movie_index = pivot_table.index.get_loc(movie_name)
     similar_scores = similarity_matrix[movie_index]
     similar_movies = list(pivot_table.iloc[similar_scores.argsort()][:6].index)
     similar_movies.remove(movie_name)
-    return similar_movies[:5]
+    
+    similar_movies_with_mean = []
+    for movie in similar_movies:
+        mean_rating = df[df['Filme'] == movie]['Avaliação'].mean()
+        similar_movies_with_mean.append((movie, mean_rating))
+
+    return similar_movies_with_mean[:5]
 
 if st.button("Mostrar recomendações"):
-    recommendations = get_recommendations_euclidean(selected_movie, similarity_matrix)
+    recommendations = get_recommendations_euclidean(selected_movie, similarity_matrix, pivot_table)
+    recommendations_df = pd.DataFrame(recommendations, columns=['Filme Recomendado', 'Média de Avaliação'])
     st.write("Filmes recomendados:")
-    st.dataframe(recommendations)
+    st.dataframe(recommendations_df)
